e4c208f363f615cbdeb087b350dc286169892034
Denys Rógeres <dennysrogeres@gmail.com>
2022-04-03 18:45:42 -0300

    criando tela de login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,48 @@
+import { Button, Flex, Input, Stack, FormControl, FormLabel } from '@chakra-ui/react'
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
+      <Flex as="form" w="100%" maxWidth={360} bg="gray.800" p="8" borderRadius={8} flexDirection="column">
+        
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+
+            <Input 
+              id="email"
+              name="email" 
+              type="email" 
+              focusBorderColor="pink.500" 
+              bgColor="gray.900"
+              variant="filled"
+              size="lg"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+
+            <Input 
+              id="password"
+              name="password" 
+              type="password" 
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              size="lg"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+            />
+          </FormControl>
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
+      </Flex>
+    </Flex>
   )
 }
