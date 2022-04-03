0ec25204bf1e36c5059af31e054326c68061b7f3
Denys Rógeres <dennysrogeres@gmail.com>
2022-04-03 19:00:44 -0300

    criando component input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { Button, Flex, Input, Stack, FormControl, FormLabel } from '@chakra-ui/react'
+import { Button, Flex, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
 
 export default function Home() {
   return (
@@ -6,39 +7,8 @@ export default function Home() {
       <Flex as="form" w="100%" maxWidth={360} bg="gray.800" p="8" borderRadius={8} flexDirection="column">
         
         <Stack spacing="4">
-          <FormControl>
-            <FormLabel htmlFor="email">E-mail</FormLabel>
-
-            <Input 
-              id="email"
-              name="email" 
-              type="email" 
-              focusBorderColor="pink.500" 
-              bgColor="gray.900"
-              variant="filled"
-              size="lg"
-              _hover={{
-                bgColor: 'gray.900'
-              }}
-            />
-          </FormControl>
-
-          <FormControl>
-            <FormLabel htmlFor="password">Senha</FormLabel>
-
-            <Input 
-              id="password"
-              name="password" 
-              type="password" 
-              focusBorderColor="pink.500"
-              bgColor="gray.900"
-              variant="filled"
-              size="lg"
-              _hover={{
-                bgColor: 'gray.900'
-              }}
-            />
-          </FormControl>
+          <Input name="email" type="email" label="E-mail"/>
+          <Input name="password" type="password" label="Senha"/>
         </Stack>
 
         <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
